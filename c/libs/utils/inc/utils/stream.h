ef9c8ed5bc6dbba26b26817a76cc864fd00ea083
Sam Coppini <samcoppini@gmail.com>
2019-05-10 22:55:59 -0700

    Add Stream class

---
+++ c/libs/utils/inc/utils/stream.h
@@ -0,0 +1,20 @@
+#ifndef UTILS_STREAM_H
+#define UTILS_STREAM_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct Stream Stream;
+struct String;
+
+Stream *stream_from_string(const struct String *str);
+
+Stream *stream_from_file(FILE *file);
+
+void free_stream(Stream *stream);
+
+bool stream_ended(const Stream *stream);
+
+char stream_get_char(Stream *stream);
+
+#endif
---
+++ c/libs/utils/src/stream.c
@@ -0,0 +1,55 @@
+#include "utils/stream.h"
+#include "utils/string.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Stream {
+    char *chars;
+
+    size_t cur_index;
+
+    size_t len;
+};
+
+Stream *stream_from_string(const String *str) {
+    Stream *stream = malloc(sizeof(Stream));
+    stream->chars = malloc(sizeof(char) * string_len(str));
+    stream->cur_index = 0;
+    stream->len = string_len(str);
+    memcpy(stream->chars, string_data(str), stream->len);
+    return stream;
+}
+
+Stream *stream_from_file(FILE *file) {
+    String *str = new_string();
+
+    int c;
+
+    while ((c = fgetc(file)) != EOF) {
+        string_add_char(str, c);
+    }
+
+    Stream *stream = stream_from_string(str);
+    free_string(str);
+    return stream;
+}
+
+void free_stream(Stream *stream) {
+    free(stream->chars);
+    free(stream);
+}
+
+bool stream_ended(const Stream *stream) {
+    return stream->cur_index == stream->len;
+}
+
+char stream_get_char(Stream *stream) {
+    if (stream_ended) {
+        return 0;
+    }
+    else {
+        return stream->chars[stream->cur_index++];
+    }
+}
