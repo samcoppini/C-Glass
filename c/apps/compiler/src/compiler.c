03d416139ec3e08bb37429e5ed6f2b655a26eb95
Sam Coppini <samcoppini@gmail.com>
2019-09-16 07:14:07 -0700

    Generate class definitions

--- c/apps/compiler/src/compiler.c
+++ c/apps/compiler/src/compiler.c
@@ -73,6 +73,7 @@ void generate_name_enum(String *code, const Map *classes) {
         string_add_char(code, ',');
     }
 
+    string_add_chars(code, "    NUM_NAMES\n");
     string_add_chars(code, "\n};\n\n");
 
     free_set(name_set);
@@ -121,11 +122,45 @@ void generate_functions(String *code, const Map *classes) {
     free_list(class_names);
 }
 
+void generate_class_definitions(String *code, const Map *classes) {
+    List *class_names = map_get_keys(classes);
+
+    for (size_t i = 0; i < list_len(class_names); i++) {
+        const String *class_name = list_get(class_names, i);
+        const GlassClass *gclass = map_get(classes, class_name);
+        List *func_names = class_get_func_names(gclass);
+
+        string_add_chars(code, "const GlassClass C_");
+        string_add_str(code, class_name);
+        string_add_chars(code, " = {\n    {\n");
+
+        for (size_t j = 0; j < list_len(func_names); j++) {
+            const String *func_name = list_get(func_names, j);
+            String *mangled_name = mangle_name(class_name, func_name);
+
+            string_add_chars(code, "        [NAME_");
+            string_add_str(code, func_name);
+            string_add_chars(code, "] = ");
+            string_add_str(code, mangled_name);
+            string_add_chars(code, ",\n");
+
+            free_string(mangled_name);
+        }
+
+        string_add_chars(code, "    }\n};\n\n");
+
+        free_list(func_names);
+    }
+
+    free_list(class_names);
+}
+
 String *compile_classes(const Map *classes) {
     String *code = new_string();
 
     generate_name_enum(code, classes);
     add_runtime_library(code);
+    generate_class_definitions(code, classes);
     generate_functions(code, classes);
 
     return code;
