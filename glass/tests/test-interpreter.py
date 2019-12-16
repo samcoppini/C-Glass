643f1f52be1e63f7226cca18925aba5feb44e0e9
Sam Coppini <samcoppini@gmail.com>
2019-12-15 20:28:33 -0800

    Make compiler tests actually work

--- glass/tests/test-interpreter.py
+++ glass/tests/test-interpreter.py
@@ -4,7 +4,7 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--exe', type=str, help='Path to the executable')
-parser.add_argument('sources', nargs='+', type=str, help='Path to the glass source file')
+parser.add_argument('sources', nargs='*', type=str, help='Path to the glass source file')
 parser.add_argument('--expected', type=str, help='Path to the expected output')
 args = parser.parse_args()
 
@@ -13,7 +13,9 @@ stdout, stderr = process.communicate()
 
 with open(args.expected, 'rb') as file:
     expected = file.read()
-    if stdout != expected:
+    if stdout == expected:
+        print('Test output matched expected output!')
+    else:
         print('---Expected Output---')
         print(expected)
         print('---Actual Output---')
